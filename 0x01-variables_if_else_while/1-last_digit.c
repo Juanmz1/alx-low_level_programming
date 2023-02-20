ddc3a7e873a4a114cc3a00aa46ab8708fa004f15
juanmz1 <juanmzii48@gmail.com>
2023-02-20 13:44:12 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,29 +3,32 @@
 #include <stdio.h>
 
 /**
- * main - Print the last digit of the number
+ * main - annoying 
  *
  * Return: 0
 */
+
 int main(void)
 {
-	int n;
-	int x;
+        int n;
+        int x;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	x = n % 10;
-	printf("Last digit of %d is %d ", n, x);
-	if (x > 5)
+        srand(time(0);
+        n = rand() - RAND_MAX / 2;
+        x = n % 10;
+        printf("Last digit of %d is %d ", n, x);
+        if (x > 5)
+        {
+                printf(" and is greater than 5");
+        }
+        if (x == 0)
+        { 
+                printf(" and is 0");
+        } 
+       	if (x < 6 && x != 0)
 	{
-		printf(" and is greater than 5");
-	}
-	if (x == 0)
-	{
-		printf(" and is 0");
-	}
-	if (x < 6 && x != 0)
-		printf(" and is less than 6 and not 0");
-	printf("\n");
+       		printf(" and is less than 6and not 0")
+	}	
+       	printf("\n");
 	return (0);
 }
