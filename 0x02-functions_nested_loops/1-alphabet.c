ec86508657b0430e012dbb5aa15fd92b9a00674b
juanmz1 <juanmzii48@gmail.com>
2023-02-22 03:07:04 -0800

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print the alphabet in lowercase
+ * print_alphabet - Print all alphabet in lowercase
  *
 */
-print_alphabet(void)
+void print_alphabet(void)
 {
 	char letter;
 
