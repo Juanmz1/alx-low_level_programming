8bd01399e49a2031745495827cb8f48a924f5893
juanmz1 <juanmzii48@gmail.com>
2023-02-23 16:29:49 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - print num from 0 to 9
+ * print_numbers - print 0123456789
+ * owned by juanm
+ *
  * Return: void
 */
 void print_numbers(void)
 {
 	char c;
 
-	for (c =  '0'; c <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 	_putchar(c);
 	}
