85b46efaeab5be66bf2fc3b32b109d6fc4c9565b
juanmz1 <juanmzii48@gmail.com>
2023-03-01 15:42:01 -0800

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,15 @@ char *_strcat(char *dest, char *src)
 	int i;
 
 	while (dest[len] != '\0')
+	{
 		dest++;
+	}
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[len] = src[i];
 		len += 1;
 	}
 	dest[len] = '\0';
-	return (dest);
+	return (len);
 }
 
