b91df0a9766efe6a128494618d767bde22ddefbe
juanmz1 <juanmzii48@gmail.com>
2023-03-06 13:24:38 -0800

    task7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,18 @@
 #include "main.h"
-/**
- * print_diagsums - prints the sums of the two diagonals of a square matrix
- * @a: pointer to start of matrix
- * @size: width of matrix column
- *
- * Return: void
- */
-void print_diagsums(int *a, int size)
-{
-	int i, j, p, l = 0, r = 0;
 
-	for (i = 0; i < size; i++)
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: array
+ * Return: 0
+*/
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
 	{
-		p = (i * size) + i;
-		l += *(a + p);
+		for (c = 0; c < 8; c++)
+			_putchar(a[b][c]);
 	}
-	for (j = 0; j < size; j++)
-	{
-		p = (j * size) + (size - 1 - j);
-		r += *(a + p);
-	}
-	_putchar("%i, %i\n", l, r);
+	_putchar('\n');
 }
