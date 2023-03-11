739032d97bdbba685565f449f30ef21b4eb9bf41
juanmz1 <juanmzii48@gmail.com>
2023-03-10 18:58:55 -0800

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that adds positive numbers.
+ * @argc: arguments count
+ * @argv: arrays of arguments
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	char *flag;
+	int i, n, sum;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
+
+
