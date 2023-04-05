d58d1a4601964cfdd3d431359a82e5977e669443
juanmz1 <juanmzii48@gmail.com>
2023-04-05 07:52:13 -0700

    task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,13 +12,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	listint_t *current = *h
 
-	while (h != NULL)
+	while (h)
 	{
 		count++;
 		printf("%d\n", h->n);
-		current = h->next;
+		h = h->next;
 	}
 	return (count);
 }
