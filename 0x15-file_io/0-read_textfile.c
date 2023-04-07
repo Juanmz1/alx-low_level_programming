756535c9d8a6e5f72a199e6480e7ea44b2ee759a
juanmz1 <juanmzii48@gmail.com>
2023-04-07 08:16:52 -0700

    task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - function that reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: a pointer to a const char
+ * @letters: a size_t to print and read the number of letter
+ * Return: if write fails or does not write
+ * the expected amount of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t read, written;
+	FILE *fp;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fp = fopen("filename", "r");
+	if (fp == NULL)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) *letters);
+	if (buffer == NULL)
+	{
+		fclose(fp);
+		return (0);
+	}
+	read = fread(buffer, sizeof(char), letters, fp);
+	if (read == -1)
+	{
+		fclose(fp);
+		free(buffer);
+		return (0);
+	}
+	written = fwrite(buffer, sizeof(char), read, stdout);
+	if (written != read)
+	{
+		fclose(fp);
+		free(buffer);
+		return (0);
+	}
+	fclose(fp);
+	free(buffer);
+
+	return (read);
+}
---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - print out char
+ * @c: char to be printed
+ * Return: 0
+*/
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return 0;
+}
