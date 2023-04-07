0c52aae2f9bae7b102e45e4a74a6471981de7430
juanmz1 <juanmzii48@gmail.com>
2023-04-07 09:48:11 -0700

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,8 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++);
+		for (len = 0; text_content[len]; len++)
+			;
 	}
 	read = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	written = write(read, text_content, len);
@@ -27,6 +28,5 @@ int create_file(const char *filename, char *text_content)
 		close(read);
 		return (-1);
 	}
-	close(read);
 	return (1);
 }
