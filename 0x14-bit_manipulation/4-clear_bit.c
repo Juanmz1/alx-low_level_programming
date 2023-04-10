27099a777cc9a3984b7bb3ad321cbe8b60d9a84a
juanmz1 <juanmzii48@gmail.com>
2023-04-10 07:06:14 -0700

    task

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int rev;
 
-	if (index > (sizeof(unsigned long int) * 8) - 1)
+	if (index > (sizeof(unsigned long int) * 8 - 1)
 		return (-1);
 	rev = ~(1 << index);
 	*n = *n & rev;
+
 	return (1);
 }
-
