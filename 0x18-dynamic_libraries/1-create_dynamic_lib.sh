b4a4ea4b42202d29b5502d8e1519352c8acdae64
juanmz1 <juanmzii48@gmail.com>
2023-05-22 07:25:08 -0700

    task

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc *.c -c fPIC
-gcc *.o -shared -o liball.so
+gcc -wall -wextra -werror -pedantic -c fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=:/root/alx-low_level_programming/0x18-dynamic_libraries
