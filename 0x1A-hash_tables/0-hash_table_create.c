8840a17fae48bd70a6600432ef59a9df035a7626
juanmz1 <juanmzii48@gmail.com>
2023-06-14 09:19:00 -0700

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size);
 {
-	unsigned long int size;
-	hash_table_t *table;
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t *));
-	if (table->array == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(table);
+		free(hash_table);
 		return (NULL);
 	}
-	for (int i = 0; i < size[i]; i++)
-	{
-		table->array[i] == NULL;
-		return (table);
-	}
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	return (hash_table)
 }
